75004f21f4dd23a854ef94ff11f1442fa692f383
Froxyy-dev <mateusz.winiarek1@gmail.com>
2023-07-11 18:44:07 +0200

    Implement user register feature and correct references to user.

--- account/urls.py
+++ account/urls.py
@@ -6,4 +6,5 @@ app_name = 'account'
 urlpatterns = [
     path('login/', views.login_page, name="login"),
     path('logout/', views.logout_user, name="logout"),
+    path('register/', views.register_page, name="register"),
 ]
--- account/views.py
+++ account/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from base.models import User
-
+from django.contrib.auth import authenticate, login, logout
+from .forms import CreateUser
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 
 
 def login_page(request):
+    page = 'login'
+
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
-            user = User.objects.get(username=username)
+            user = get_user_model().objects.get(username=username)
         except:
             messages.error(request, 'Nie ma takiego użytkownika.')
 
@@ -25,7 +27,31 @@ def login_page(request):
         else:
             messages.error(request, 'Nazwa użytkownika lub hasło się nie zgadza.')
 
-    context = {}
+    context = {'page': page}
+    return render(request, 'account/login_register.html', context)
+
+
+def register_page(request):
+    page = 'register'
+    form = CreateUser()
+
+    if request.method == 'POST':
+        form = CreateUser(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            login(request, user)
+            return redirect('home:home')
+        else:
+            messages.error(request, 'Wystąpił błąd podczas rejestracji.')
+
+    context = {
+        'page': page,
+        'form': form,
+    }
     return render(request, 'account/login_register.html', context)
 
 
