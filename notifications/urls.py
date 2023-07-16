9e5baf8d884d3bf418bf3b92863b0159ad0c4c48
Mateusz Jacniacki <mateuszjacniacki2@gmail.com>
2023-07-16 21:37:01 +0200

    Improve notifications.

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,2 +1,7 @@
 from django.urls import path
+from . import views
+app_name = 'notifications'
 
+urlpatterns = [
+    path("read_all", views.read_all_notifications, name="read_all"),
+]
--- notifications/views.py
+++ notifications/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from notifications.utils import prepare_notifications
 from django.template.response import TemplateResponse
-
+from django.contrib.auth.decorators import login_required
+from notifications.models import Notification
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponseRedirect, HttpResponse
+import json
 # Create your views here.
 
 
@@ -14,3 +19,21 @@ def show_notifications(view):
         else:
             return r
     return wrapper
+
+@login_required(login_url='account:login')
+def read_notification(request, pk):
+    notification = get_object_or_404(Notification, pk=pk)
+    if notification.user != request.user:
+        raise PermisionDenied()
+    notification.is_read = True
+    notification.save()
+
+@login_required(login_url='account:login')
+def read_all_notifications(request):
+    Notification.objects.filter(user=request.user).update(is_read=True)
+    response_data = {}
+    response_data['result'] = "success"
+    return HttpResponse(
+        json.dumps(response_data),
+        content_type="application/json"
+    )
