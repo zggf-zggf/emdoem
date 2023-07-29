1e039db3b9ca3f0aca96c2ad2cc12b910a654424
Froxyy-dev <mateusz.winiarek1@gmail.com>
2023-07-29 12:02:34 +0200

    Remove python 3.10 match instruction.

--- notifications/utils.py
+++ notifications/utils.py
@@ -2,6 +2,7 @@ from base.models import Comment, Solution
 from notifications.models import NewCommentNotification, Notification, NewSolutionNotification
 from base.utils import get_watchers_of_problem
 
+
 def notify_new_comment(comment):
    notification = NewCommentNotification( type="comment",
                                          comment=comment,
@@ -9,6 +10,7 @@ def notify_new_comment(comment):
                                          name=("@"+comment.user.username+" skomentował twoje rozwiązanie..."),
                                          content=("Użytkownik @"+comment.user.username+" dodał komentarz do twojego rozwiązania w zadaniu \""+comment.solution.problem.name+"\""))
    notification.save()
+
 
 def notify_new_solution(solution):
     watchers = get_watchers_of_problem(solution.problem)
@@ -21,13 +23,22 @@ def notify_new_solution(solution):
                                           content=("Użytkownik @"+solution.user.username+" dodał rozwiązanie do zadania \""+solution.problem.name+"\""))
         notification.save()
 
+
 def attach_urls(notifications):
     for notification in notifications:
-        match notification.type:
-            case "comment":
-                setattr(notification, 'url', notification.newcommentnotification.get_url())
-            case "solution":
-                setattr(notification, 'url', notification.newsolutionnotification.get_url())
+        if notification.type == "comment":
+            setattr(notification, 'url', notification.newcommentnotification.get_url())
+        elif notification.type ==  "solution":
+            setattr(notification, 'url', notification.newsolutionnotification.get_url())
+
+
+    # for notification in notifications:
+    #     match notification.type:
+    #         case "comment":
+    #             setattr(notification, 'url', notification.newcommentnotification.get_url())
+    #         case "solution":
+    #             setattr(notification, 'url', notification.newsolutionnotification.get_url())
+
 
 def prepare_notifications(user):
     if user.is_authenticated:
@@ -35,6 +46,7 @@ def prepare_notifications(user):
         notifications = Notification.objects.filter(user=user).order_by('-creation_date')[0 : 3]
         attach_urls(notifications)
         return {'count': count, 'notifications': notifications}
+
 
 def prepare_all_notifications(user):
     if user.is_authenticated:
