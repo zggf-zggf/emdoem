3b795c0cdb9c4d397eda68f5a49a841abfd8f58f
Mateusz Jacniacki <mateuszjacniacki2@gmail.com>
2023-08-01 09:17:59 +0200

    Introduce PyInvoke

--- tasks.py
+++ tasks.py
@@ -4,5 +4,8 @@ def update(c):
     c.run('git pull')
     c.run('python manage.py collectstatic')
     c.run('python manage.py migrate')
+    print('restarting gunicorn...')
     c.sudo('systemctl restart gunicorn')
+    print('restarting nginx...')
     c.sudo('systemctl restart nginx')
+    print('done!')
